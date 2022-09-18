073d7878375b60f0ff978ce40943935eaffde85b
xalencicx <alenmmmujkic92@gmail.com>
2022-09-18 04:20:19 +0300

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Alen Mujkic on 18.09.22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
