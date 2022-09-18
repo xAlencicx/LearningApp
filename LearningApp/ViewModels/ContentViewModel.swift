915a2a0bc3b8fb9f44dc668d3d2bf0df5086c083
xalencicx <alenmmmujkic92@gmail.com>
2022-09-18 04:26:34 +0300

    - rename content view to home view - organise files into folders - add content view model - set content view model as environment object

---
+++ LearningApp/ViewModels/ContentViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentViewModel.swift
+//  LearningApp
+//
+//  Created by Alen Mujkic on 18.09.22.
+//
+
+import Foundation
+
+class ContentViewModel : ObservableObject {
+    
+}
--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -7,7 +7,10 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
+    
+    @EnvironmentObject var contentViewModel : ContentViewModel
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -21,6 +24,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
